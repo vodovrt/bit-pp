48d466b917ffbd16305e55e1c589aa77e0de017e
pera <classroom@bgit.rs>
2020-10-06 13:06:16 +0200

    poruka

---
+++ switch/zadatak5.js
@@ -0,0 +1,50 @@
+var input = "mama"
+
+switch (input) {
+
+
+
+
+    case "januar":
+    case "februar":
+    case "mart":
+        {
+            console.log("1");
+            break;
+        }
+    case "april":
+    case "maj":
+    case "jun":
+        {
+            console.log("2");
+            break;
+        }
+
+    case "jul":
+    case "avgust":
+    case "septembar":
+        {
+            console.log("3");
+            break;
+        }
+    case "oktobar":
+    case "novembar":
+    case "decembar":
+        {
+            console.log("4")
+            break;
+        }
+
+
+    default: {
+        console.log("поруку која објашњава да унос мора бити број између 1 и 12.")
+
+
+    }
+
+
+
+
+
+
+}
