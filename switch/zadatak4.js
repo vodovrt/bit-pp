7dfabee2205660bb9585aa5475736f91ddc89495
pera <classroom@bgit.rs>
2020-10-06 12:43:35 +0200

    resenje

---
+++ switch/zadatak4.js
@@ -0,0 +1,62 @@
+var input = 8
+
+switch (input) {
+    case 1: {
+        console.log("januar");
+        break;
+    }
+    case 2: {
+        console.log("februar");
+        break;
+    }
+    case 3: {
+        console.log("mart");
+        break;
+    }
+    case 4: {
+        console.log("april");
+        break;
+    }
+    case 5: {
+        console.log("maj");
+        break;
+    }
+    case 6: {
+        console.log("jun");
+        break;
+    }
+    case 7: {
+        console.log("jul");
+        break;
+    }
+    case 8: {
+        console.log("avgust");
+        break;
+    }
+    case 9: {
+        console.log("septembar");
+        break;
+    }
+    case 10: {
+        console.log("oktobar");
+        break;
+    }
+    case 11: {
+        console.log("novembar");
+        break;
+    }
+    case 12: {
+        console.log("decembar");
+        break;
+    }
+    default: {
+        console.log("Улаз мора бити број између 1 и 12")
+    }
+
+
+
+
+
+}
+
+
